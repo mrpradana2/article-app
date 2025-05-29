4c14aed6488efa5ed29df59fd9c3d942a9b648e4
mrpradana2 <mrpradana2@gmail.com>
2025-05-29 12:54:25 +0700

    fix: move article page to directory/slug/page.js

--- src/app/articles/[slug]/page.js
+++ src/app/articles/[slug]/page.js
@@ -1,16 +1,17 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 
 export default function ArticlePage() {
   const params = useParams();
-  const { id } = params;
+  const slug = params.slug;
 
   return (
     <>
       <section className="flex flex-col gap-y-2">
         <div>
-          <p>{id}</p>
+          <p>{slug}</p>
           <h1></h1>
           <p></p>
         </div>
